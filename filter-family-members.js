f591dd3de566f6dfbe1c8d93a44395fb4c527ba7
doug <dougecox@gmail.com>
2017-03-29 11:22:03 -0700

    Add files via upload

---
+++ filter-family-members.js
@@ -0,0 +1,97 @@
+// `filterFamilyMembers` accepts two arguments, a family tree object, and a truth test. `filterFamilyMembers` 
+// returns an array, in any order, of the full names of family members who pass the passed in truth test.
+// You will need to use recursion in your solution in order to handle nested family trees.
+//
+// A family member looks like this:
+//
+// {
+//   firstName: 'Fred'
+//   lastName: 'Zirdung'
+//   location: 'San Francsico'
+//   children: [/* ... */]
+// }
+//
+// EXAMPLE:
+//
+var familyTree = {
+  'firstName': 'Beth',
+  'lastName': 'Johnson',
+  'location': 'San Francisco',
+  'children': [
+    {
+      'firstName': 'Beth Jr.',
+      'lastName': 'Johnson',
+      'location': 'Berkeley',
+      'children': [
+        {
+          'firstName': 'Smitty',
+          'lastName': 'Won',
+          'location': 'Beijing',
+          'children': []
+        }
+      ]
+    },
+    {
+      'firstName': 'Joshie',
+      'lastName': 'Wyattson',
+      'location': 'Berkeley',
+      'children': []
+    }
+  ]
+};
+
+var livesInBerkeley = function (familyMember) {
+  return familyMember.location === 'Berkeley';
+}
+//
+// filterFamilyMembers(familyTree, livesInBerkeley)
+//
+// returns ['Beth Jr. Johnson', 'Joshie Wyattson'];
+
+
+
+var filterFamilyMembers = function (familyTree, truthTest) {
+	// base case does this obj meet base case
+	var results = []
+	if ( truthTest(familyTree) ) {
+		var childName = familyTree.firstName + " " + familyTree.lastName;
+        results.push(childName);
+    }
+	familyTree.children.forEach( function( child ) {
+			results.push( filterFamilyMembers(child, truthTest) );
+		});
+	return results;
+};
+
+
+
+  
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
